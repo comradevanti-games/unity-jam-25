f9ec943518c4495852968897d37c7feb2ae7ddff
Ramon Brullo <ramon.brullo@proton.me>
2025-11-08 14:59:22 +0100

    movement burns energy

--- Assets/Code/Motor/ForwardMovementMotor.cs
+++ Assets/Code/Motor/ForwardMovementMotor.cs
@@ -4,8 +4,10 @@ public class ForwardMovementMotor : MonoBehaviour, IMotor
 {
     [SerializeField] private float maxMovementForce;
     [SerializeField] private float turnForce;
+    [SerializeField] private float energyBurnRate;
 
     private new Rigidbody rigidbody = null!;
+    private EnergyHandler energyHandler = null!;
 
     public void MoveIn(Vector3 direction)
     {
@@ -14,16 +16,23 @@ public class ForwardMovementMotor : MonoBehaviour, IMotor
 
         var force = maxMovementForce * scale;
         rigidbody.AddForce(transform.forward * force, ForceMode.Force);
+
+        var burnedEnergy = force * energyBurnRate * Time.fixedDeltaTime;
+        energyHandler.OnEnergyBurned(burnedEnergy);
     }
 
     public void TurnIn(float direction)
     {
         rigidbody.AddTorque(Vector3.up * (direction * turnForce),
             ForceMode.Force);
+
+        var burnedEnergy = turnForce * Time.fixedDeltaTime;
+        energyHandler.OnEnergyBurned(burnedEnergy);
     }
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        energyHandler = FindAnyObjectByType<EnergyHandler>();
     }
 }
--- Assets/Code/Motor/IMotor.cs
+++ Assets/Code/Motor/IMotor.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public interface IMotor
 {
+    /// <summary>
+    /// Applies lateral force in the given direction. Call this in FixedUpdate.
+    /// </summary>
     public void MoveIn(Vector3 direction);
 
+    /// <summary>
+    /// Applies angular force in the given direction. Call this in FixedUpdate.
+    /// </summary>
     public void TurnIn(float direction);
 }
