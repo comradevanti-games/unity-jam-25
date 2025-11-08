9ec7ac144fd2a106dfdc51e3e0adb024e97b3a2f
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2025-11-08 08:26:54 +0100

    add game input lock

--- Assets/Code/Game/GameHandler.cs
+++ Assets/Code/Game/GameHandler.cs
@@ -1,3 +1,12 @@
+using System;
 using UnityEngine;
 
-public class GameHandler : MonoBehaviour { }
+public class GameHandler : MonoBehaviour {
+
+    public event Action<bool>? GameInitialized;
+
+    private void Start() {
+        GameInitialized?.Invoke(true);
+    }
+
+}
--- Assets/Code/Inputs/InputHandler.cs
+++ Assets/Code/Inputs/InputHandler.cs
@@ -6,7 +6,19 @@ public class InputHandler : MonoBehaviour {
 
     public event Action<Vector2>? MovementInputHandled;
 
+    private bool gameInputLocked = true;
+
+    private void Start() {
+        FindAnyObjectByType<GameHandler>().GameInitialized += OnGameInitialized;
+    }
+
+    private void OnGameInitialized(bool isInitialized) {
+        gameInputLocked = !isInitialized;
+    }
+
     public void OnMovementInputReceived(InputAction.CallbackContext ctx) {
+
+        if (gameInputLocked) return;
 
         if (ctx.performed) {
             MovementInputHandled?.Invoke(ctx.ReadValue<Vector2>());
