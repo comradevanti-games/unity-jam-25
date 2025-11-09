d2c39e69f25a06e5926a7b8942d34ac69d48daf8
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2025-11-09 09:51:17 +0100

    add new nutrient models

--- Assets/Code/Game/Nutrient.cs
+++ Assets/Code/Game/Nutrient.cs
@@ -1,11 +1,54 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Nutrient : MonoBehaviour {
 
     public event Action<Nutrient>? Consumed;
 
     public float energy = 2f;
+
+    [SerializeField] private GameObject[] possibleNutrientForms = null;
+    [SerializeField] private float minAngularSpeed = 10f;
+    [SerializeField] private float maxAngularSpeed = 50f;
+    [SerializeField] private float minRotationDuration = 2f;
+    [SerializeField] private float maxRotationDuration = 5f;
+
+    private Rigidbody rb;
+    private float rotationTimer;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+
+        int randomNutrient = Random.Range(0, possibleNutrientForms.Length);
+
+        for (int i = 0; i < possibleNutrientForms.Length; i++) {
+            possibleNutrientForms[i].SetActive(i == randomNutrient);
+        }
+
+        PickNewRotation();
+
+    }
+
+    private void Update() {
+
+        rotationTimer -= Time.deltaTime;
+
+        if (rotationTimer <= 0f) {
+            PickNewRotation();
+        }
+
+    }
+
+    private void PickNewRotation() {
+        Vector3 randomAxis = Random.onUnitSphere;
+        float randomSpeed = Random.Range(minAngularSpeed, maxAngularSpeed);
+        rotationTimer = Random.Range(minRotationDuration, maxRotationDuration);
+        rb.angularVelocity = randomAxis * randomSpeed;
+    }
 
     private void OnTriggerEnter(Collider other) {
         Consumed?.Invoke(this);
