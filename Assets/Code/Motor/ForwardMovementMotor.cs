741bc049369a269567d1acf5161c77d238cb2384
Ramon Brullo <ramon.brullo@proton.me>
2025-11-09 08:40:00 +0100

    increase tail motion angle

--- Assets/Code/Motor/ForwardMovementMotor.cs
+++ Assets/Code/Motor/ForwardMovementMotor.cs
@@ -18,7 +18,7 @@ public class ForwardMovementMotor : MonoBehaviour, IMotor
     public void MoveIn(Vector3 direction)
     {
         var scale = Vector3.Dot(transform.forward, direction);
-        if (scale < 0.75) return;
+        if (scale < 0.5) return;
 
         var force = maxMovementForce * scale;
         rigidbody.AddForce(transform.forward * force, ForceMode.Force);
